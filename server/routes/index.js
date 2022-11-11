4630fed76df97c6cf1e9cd7357217c785271e206
Alex <azuniga1@my.centennialcollege.ca>
2022-11-10 22:49:20 -0500

    first commit - basic folder architecture, route, view, server etc.

---
+++ server/routes/index.js
@@ -0,0 +1,20 @@
+let express = require('express');
+//const surveys = require('../models/surveys');
+const router = express.Router();
+
+//create models
+// const reservation = require('../models/reservations')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+          console.log('home')
+          res.render('home', {
+            title: 'Restaurants',
+          });
+});
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Restaurants'});
+});
+
+module.exports = router;
