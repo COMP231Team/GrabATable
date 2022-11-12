c577d296ee05ef4c2cf46f250f2544e8ce2c8985
abishekh-manoharan <110298717+abishekh-manoharan@users.noreply.github.com>
2022-11-12 11:36:21 -0500

    added reservations router support

--- server/config/app.js
+++ server/config/app.js
@@ -16,6 +16,7 @@ mongoDB.once('open', ()=> {
 });
 
 let indexRouter = require('../routes/index');
+let reservationRouter = require('../routes/reservations');
 
 let app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 
 app.use('/', indexRouter);
+app.use('/reservations', reservationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let router = express.Router();
-let mongoose = express.mongoose();
+let mongoose = express.mongoose;
 
 let reservation = require('../models/reservations');
 
+router.get('/', (req, res) => {
+    //res.render('', { '});
+})
 
-
+module.exports = router;
 
