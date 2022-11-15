e8aab2aad96ddcfaee6e86031619732e8a8706a2
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-11-15 16:52:23 -0500

    Add Routes and Controllers for Restaurant Pages

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,4 @@ let controllerIndex = require('../controllers/index');
 // Routes
 router.get('/', controllerIndex.home);
 
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Restaurants'});
-});
-router.get('/edit', function(req, res, next) {
-  res.render('editRestaurant', { title: 'edit'});
-});
-router.get('/book', function(req, res, next) {
-  res.render('booking', { title: 'book'});
-});
-
 module.exports = router;
