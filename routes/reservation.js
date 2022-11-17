75283551be9dd60d4eba22ce6be9703343c7855e
abishekh-manoharan <110298717+abishekh-manoharan@users.noreply.github.com>
2022-11-17 10:18:10 -0500

    added booking page router to reservation.js

--- routes/reservation.js
+++ routes/reservation.js
@@ -4,6 +4,9 @@ let router = express.Router();
 let controllerReservations = require('../controllers/reservations');
 
 // Routes
+// Get booking page
+router.get('/book', controllerReservations.book);
+
 
 module.exports = router;
 
