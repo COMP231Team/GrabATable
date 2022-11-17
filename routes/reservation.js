493a5abea831e5808336902ca1e2bcfb5d9e4390
abishekh-manoharan <110298717+abishekh-manoharan@users.noreply.github.com>
2022-11-17 10:56:16 -0500

    fixed error with naming of a file

---
+++ routes/reservation.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let router = express.Router();
+
+let controllerRestaurant = require('../controllers/reservations');
+
+// Routes
+ 
+
+module.exports = router;
+
