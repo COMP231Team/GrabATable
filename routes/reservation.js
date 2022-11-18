b245dbafe3a3723d9faeae7436e9fae9f69b9ae7
abishekh-manoharan <110298717+abishekh-manoharan@users.noreply.github.com>
2022-11-18 09:10:32 -0500

    re organized routes, controllers

--- routes/reservation.js
+++ routes/reservation.js
@@ -4,7 +4,16 @@ let router = express.Router();
 let controllerRestaurant = require('../controllers/reservations');
 
 // Routes
- 
+// Get reservation list
+router.get('/list', controllerRestaurant.reservationList);
+
+// Get reservation booking form
+router.get('/book', controllerRestaurant.book);
+
+// ------------------------
+// TODO : modify reservation
+// ------------------------
+
 
 module.exports = router;
 
