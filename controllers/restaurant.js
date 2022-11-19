aab0638f8360c097f2be69cba8bb6b3986170556
0822mahesh <0822mahesh@gmail.com>
2022-11-19 17:02:41 -0500

    Edit Restaurant
    Edit Restaurant

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -15,11 +15,54 @@ exports.restaurantList = async function(req, res) {
 };
 
 // GET edit restaurant page
-exports.editRestaurant = function(req, res) {
-    res.render('restaurant/editRestaurant', { 
-        title: 'Edit Restaurant'
-    });
-};
+exports.editRestaurant = function (req, res) {
+    
+    console.log(req.method)
+    if(req.method == 'GET'){
+    Restaurant.findById(req.params.id, function(err, restaurantDetails){
+        if(err){
+            console.log(err);
+           res.end(err);
+        }else{
+            //console.log(restaurantDetails)
+            res.render('restaurant/editRestaurant', 
+            {title:'Edit Restaurant',restaurant: restaurantDetails});
+            
+        }
+    })
+}else{
+        let uName = req.body.Name;
+    let uDescription = req.body.Description;
+    let uEmail = req.body.Email;
+    let uAddress = req.body.Address;
+    let uPhone = req.body.Phone;
+    let uCapacity = req.body.Capacity;
+    let uImageUrl = req.body.ImageLinks;
+    Restaurant.findByIdAndUpdate(req.params.id,{$set:{Name : uName,
+        Description:uDescription,
+        Email:uEmail,
+        Address:uAddress,
+        Phone:uPhone,
+        Capacity:uCapacity,
+        ImageLinks:uImageUrl
+    }},(err)=>{
+        if (err) {
+            console.log(err);
+            res.end(err);
+            } else {
+            console.log('success!!');
+           // res.end();
+            res.redirect('/restaurant/list')
+            }
+    });}
+
+}
+
+
+
+
+
+
 
 // GET details page
 exports.detailsOfRestaurant = function (req, res) {
