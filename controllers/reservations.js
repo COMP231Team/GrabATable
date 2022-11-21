e28570f410312fbc3c168b804183b7117b78f4ca
Ilyas-Ismail <ilyasismaiil@hotmail.com>
2022-11-20 22:04:58 -0500

    Edit Booking
    Allows user to edit their booking based on the reservation "_id" attribute. Added an EJS page that resembles the design template.

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -46,6 +46,96 @@ exports.postBooking = function(req, res, next) {
     });
 }
 
-// ------------------------
-// TODO : modify reservation
-// ------------------------
+exports.getEditBooking = function (req, res, next) {
+    let id = req.params.id;
+  restaurant.findOne({ "Reservations._id": id }, (err, reservationDetails) => {
+    let i = 0;
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      reservationDetails.Reservations.every(element => {
+        let idCheck = String(element._id);
+        let idCurrent = String(id);
+        if (idCheck !== idCurrent) {
+          i++;
+          return true;
+        }
+        return false;
+      });
+      let reserve = reservationDetails.Reservations[i];
+
+      //modifies the data from the db into a form that's accepted by the ejs file
+      let cast = new Date(Date.parse(reserve.Date));
+      let fixedDate = cast.toISOString();
+      fixedDate = fixedDate.split("T");
+      res.render('reservation/editBooking', { reservation: reserve, resDate: fixedDate[0], restaurantName: reservationDetails.Name });
+    }
+  });
+}
+
+exports.postEditBooking = function (req, res, next) {
+    //needs the id of the reservation, not restaurant
+    let id = req.params.id;
+
+    let reservation = ({
+      Guest: req.body.Name,
+      Phone: req.body.Phone,
+      Email: req.body.Email,
+      Date: Date.parse(req.body.Date),
+      NumberOfGuests: parseInt(req.body.Guests),
+      Notes: req.body.Notes,
+      TablesReserved: parseInt(req.body.Guests) / 2,
+    });
+  
+    restaurant.findOne({ "Reservations._id": id }, (err, reservationDetails) => {
+      let i = 0;
+  
+      if (err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        //user wants to modify
+        if (req.body.choice == 1) {
+          reservationDetails.Reservations.every(element => {
+            let idCheck = String(element._id);
+            let idCurrent = String(id);
+            if (idCheck !== idCurrent) {
+              i++;
+              return true;
+            }
+  
+            reservationDetails.Reservations[i] = reservation;
+            reservationDetails.Reservations[i]._id = id;
+  
+            restaurant.updateOne({ "Reservations._id": id }, { $set: { Reservations: reservationDetails.Reservations } }, (err) => {
+              if (err) {
+                console.log(err);
+                res.end(err);
+              } else {
+                res.redirect('/');
+              }
+            });
+            return false;
+          });
+        }
+  
+        //user wants to cancel
+        if (req.body.choice == 2) {
+          restaurant.updateOne({ "Reservations._id": id }, { $pull: { Reservations: { _id: id } } }, (err) => {
+            if (err) {
+              console.log(err);
+              res.end(err);
+            } else {
+              res.redirect('/');
+            }
+          });
+        }
+  
+        //user wants to go back to the homepage
+        if(req.body.choice == 3) {
+          res.redirect('/');
+        }
+      }
+    });
+}
