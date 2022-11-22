fc7d1e4b0123ec8e8418efe915051bd5ff58af92
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-11-21 21:27:44 -0500

    Simplifying Restaurant Routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,5 @@
 let Restaurant = require('../models/restaurant');
 
 exports.home = function(req, res) {
-    Restaurant.find((err, restaurants) => { 
-        if (err) {
-            return console.log(err);
-        }
-        return res.render('restaurant/restaurantList', { 
-            title: 'Restaurants',
-            restaurantList: restaurants
-        });
-    });
+    return res.redirect('/restaurant');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 let express = require('express');
 const router = express.Router();
 let controllerIndex = require('../controllers/index');
-let controllerRestaurant = require('../controllers/restaurant');
 
 // Routes
-router.get('/', controllerRestaurant.restaurantList);
-router.get('/:cuisine', controllerRestaurant.filterRestaurantList);
+router.get('/', controllerIndex.home);
+
 
 module.exports = router;
