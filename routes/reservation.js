5441304a163d9d417e4588b95598322d94adf0b0
Alex <azuniga1@my.centennialcollege.ca>
2022-11-28 08:32:51 -0500

    added updated booking algorithm controllers/routes

--- routes/reservation.js
+++ routes/reservation.js
@@ -12,10 +12,12 @@ router.get('/list', controllerReservation.reservationList);
 // Customer
 // Get available timing
 router.get('/availability/:id', controllerReservation.availabilities);
+// Post available timing
+router.post('/availability/:id', controllerReservation.postAvailabilities);
 // Get reservation booking form
-router.get('/book/:id', controllerReservation.getBooking);
+router.get('/book/:id/:date/:time/:guests', controllerReservation.getBooking);
 // Post reservation booking form
-router.post("/book/:id", controllerReservation.postBooking)
+router.post("/book/:id/:date/:time/:guests", controllerReservation.postBooking)
 
 // Get reservation edit booking form
 router.get('/editBooking/:id', controllerReservation.getEditBooking);
